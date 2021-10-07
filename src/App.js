e01bd86c94edc151834bf78c6f7d951fb00c423d
Vijay Rawat <vjayrawat19@gmail.com>
2021-10-07 19:14:55 +0530

    theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from "react";
+import ThemeContext from "./Context/ThemeContext";
+import Header from "./Components/Header";
+import HeroSection from "./Components/HeroSection";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const themeHook = useState("light")
+  return(
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <Header/>
+        <HeroSection/>
+        
+      </div>
+
+    </ThemeContext.Provider>
+  )
 }
 
 export default App;
---
+++ src/Components/ThemeToggler.js
@@ -0,0 +1,25 @@
+import React, {useContext} from "react"
+import ThemeContext from "../Context/ThemeContext"
+
+const ThemeToggler = () => {
+const [themeMode, setThemeMode] = useContext(ThemeContext)
+
+return (
+    <div
+    onClick={()=>{
+        setThemeMode(themeMode === "light" ? "dark" : "light")
+    }}
+    >
+        <span>
+            {themeMode === "light" ? "Turn off" : "lights on"}
+        </span>
+
+
+
+    </div>
+)
+
+
+}
+
+export default ThemeToggler;
